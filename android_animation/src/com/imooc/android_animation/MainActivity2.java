422d782e45d9d769486a17091dc66d4cfcbfc60e
jianfang liu <jianfang liu>
2016-07-21 23:03:06 +0800

    first commit

---
+++ android_animation/src/com/imooc/android_animation/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.imooc.android_animation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity2 extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main2);
+	}
+	
+}
